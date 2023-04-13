6d9a3080e138f2327e5af58fda71f91407ad938c
Davila-Israel96 <israeldavila96@gmail.com>
2023-04-13 14:59:12 -0500

    changes to client

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import { FaUser } from "react-icons/fa";
 
 function Register() {
-	return <div>Register</div>;
+	const [formData, setFormData] = useState({
+		name: "",
+		email: "",
+		password: "",
+		password_confirm: "",
+	});
+
+	//destructure formData
+	const { name, email, password, password_confirm } = formData;
+
+	const infoChange = (e) => {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	};
+
+	return (
+		<>
+			<section>
+				<h1>
+					<FaUser />
+					Register
+				</h1>
+			</section>
+		</>
+	);
 }
 
 export default Register;
